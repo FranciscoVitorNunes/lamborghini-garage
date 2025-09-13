a6eea2ad63703e301976f4e58b7601f075bbfb26
FranciscoVitorAraujo <francisco.vitor@aluno.ufca.edu.br>
2025-09-13 20:41:46 -0300

    Função para renderizar detalhes do carro

--- src/Components/CardView/index.tsx
+++ src/Components/CardView/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Text } from 'react-native';
 
 import { styles } from './styles';
 import { Divider } from '../Divider';
 const logo = require('./../../../assets/logo.png');
 
 export function CardView() {
+
+    const renderCarDetails = ()=>(
+        <View>
+            <Text style={styles.carBrand}>Lamborghini</Text>
+            <Text style={styles.carName}>MODEL</Text>
+        </View>
+    )
+
   return (
     <View style={styles.imageContainer}>
         <View style={styles.logoContainer}>
         <Image source={logo} style={styles.imageLogo}></Image>
         </View>
         <Divider></Divider>
+        {renderCarDetails()}
     </View>
   );
 }
